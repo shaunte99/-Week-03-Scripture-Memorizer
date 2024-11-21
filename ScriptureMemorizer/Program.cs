0b7e6e7d76df75d35ae6abcfaded9c6d83043956
shaunte99 <ksmatsetela99@gmail.com>
2024-11-21 22:19:06 +0200

    Add files via upload

---
+++ ScriptureMemorizer/Program.cs
@@ -0,0 +1,36 @@
+﻿// Program_Additional.cs
+using System;
+
+public partial class Program
+{
+    public static void DisplayScripture(string scripture)
+    {
+        Console.Clear();
+        Console.WriteLine("Complete Scripture:");
+        Console.WriteLine(scripture);
+    }
+
+    public static void HideRandomWords(ref string scripture)
+    {
+        Random random = new Random();
+        string[] words = scripture.Split(' ');
+
+        
+        int index = random.Next(words.Length);
+        
+        
+        string selectedWord = words[index];
+        string hiddenWord = new string('_', selectedWord.Length);
+
+        words[index] = hiddenWord;
+
+        
+        scripture = string.Join(" ", words);
+        
+        
+        Console.Clear();
+        Console.WriteLine("Updated Scripture:");
+        Console.WriteLine(scripture);
+    }
+}
+
