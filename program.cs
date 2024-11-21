4e3cddbb4cb788ba6ef1b0a6877679f89bee89eb
shaunte99 <ksmatsetela99@gmail.com>
2024-11-21 22:19:35 +0200

    Add files via upload

---
+++ program.cs
@@ -0,0 +1,32 @@
+﻿// Program.cs
+using System;
+
+public partial class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine("Scripture Memorizer");
+
+        
+        string scripture = "John 3:16 For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.";
+
+        
+        DisplayScripture(scripture);
+
+        
+        while (true)
+        {
+            Console.WriteLine("Press Enter to hide words or type 'quit' to exit.");
+            string input = Console.ReadLine().ToLower();
+
+            if (input == "quit")
+            {
+                break;
+            }
+
+            
+            HideRandomWords(ref scripture);
+        }
+    }
+}
